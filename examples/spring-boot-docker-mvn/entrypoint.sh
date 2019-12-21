649eba724035304fdcb9c65f545bb0ef86e7132e
Gautam Pachnanda <gautampachnanda@gautams-mbp-2.home>
2019-12-21 08:23:05 +0000

    Initial repo with container example

---
+++ examples/spring-boot-docker-mvn/entrypoint.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+java -version
+ls -lah /app
+ls -lah /logs/app
+java -jar /app/app.jar
---
+++ examples/spring-boot-docker-mvn/src/test/java/org/pachnanda/springbootdockermvn/SpringBootDockerMvnApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pachnanda.springbootdockermvn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDockerMvnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
